3f25de7cb4e98b8dd18dd4c651f67b881f3b64be
AugustineKadima <kadimasam@gmail.com>
2022-10-18 20:16:37 +0300

    Add contact list

---
+++ contact.py
@@ -0,0 +1,26 @@
+class Contact:
+
+    contact_list = [] 
+
+    def __init__(self, first_name, last_name, phone_number, email):
+        self.first_name =  first_name
+        self.last_name = last_name
+        self.phone_number = phone_number
+        self.email = email
+
+    
+    def save_contact(self):
+        Contact.contact_list.append(self)
+
+    
+    def delete_contact(self):
+        Contact.contact_list.remove(self)
+
+
+    @classmethod
+    def find_by_number(cls,number):
+        for contact in cls.contact_list:
+            if contact.phone_number == number:
+                return contact
+
+    
